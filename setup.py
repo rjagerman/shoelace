b792bcc3e17a3b8da2a766403c8d69dcdaeb2118
Rolf Jagerman <rjagerman@gmail.com>
2017-06-19 14:30:46 +0200

    Update version and download link

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='lace',
-    version='0.1',
+    version='0.1.1',
     description='Neural Learning to Rank using Chainer',
     url='https://github.com/rjagerman/lace',
-    download_url = 'https://github.com/rjagerman/lace/archive/v0.1.0.tar.gz',
+    download_url = 'https://github.com/rjagerman/lace/archive/v0.1.1.tar.gz',
     author='Rolf Jagerman',
     author_email='rjagerman@gmail.com',
     license='MIT',
