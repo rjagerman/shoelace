d3fd21e06b26742ee9f681291f50e269f55f4613
Rolf Jagerman <rjagerman@gmail.com>
2017-06-19 14:28:42 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lace',
+    version='0.1',
+    description='Neural Learning to Rank using Chainer',
+    url='https://github.com/rjagerman/lace',
+    download_url = 'https://github.com/rjagerman/lace/archive/v0.1.0.tar.gz',
+    author='Rolf Jagerman',
+    author_email='rjagerman@gmail.com',
+    license='MIT',
+    packages=['lace']
+)
