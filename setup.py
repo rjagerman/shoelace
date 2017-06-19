863514e454d04e7f619017cdd1dcfe70870c56eb
Rolf Jagerman <rjagerman@gmail.com>
2017-06-19 15:01:41 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name='lace',
-    version='0.1.1',
+    version='0.1.0',
     description='Neural Learning to Rank using Chainer',
     url='https://github.com/rjagerman/lace',
-    download_url = 'https://github.com/rjagerman/lace/archive/v0.1.1.tar.gz',
+    download_url = 'https://github.com/rjagerman/lace/archive/v0.1.0.tar.gz',
     author='Rolf Jagerman',
     author_email='rjagerman@gmail.com',
     license='MIT',
     packages=['lace',
               'lace.functions',
               'lace.loss'],
-    install_requires=['numpy>=1.12.0', 'chainer>=2.0.0'],
+    install_requires=['numpy>=1.12.0',
+                      'chainer>=2.0.0'],
     tests_require=['nose']
 )
