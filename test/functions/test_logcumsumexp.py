82103c1a1aac507833d3a84714e54d91a640f353
Rolf Jagerman <rjagerman@gmail.com>
2017-06-19 11:36:53 +0200

    Add tests for logcumsumexp and listwise loss

---
+++ test/functions/test_logcumsumexp.py
@@ -0,0 +1,39 @@
+import numpy as np
+from nose.tools import assert_equal, assert_true
+from lace.functions.logcumsumexp import logcumsumexp, LogCumsumExp
+from chainer import Variable
+
+
+def test_forward():
+
+    # Construct test data
+    x = Variable(np.array([5., 3., 3., 1., 0.]))
+    expected_result = Variable(np.array([5.259069729874858, 3.7816718231374824,
+                                         3.1698460195562856, 1.313261687518223,
+                                         0.]))
+
+    # Run forward pass with shorthand notation
+    result = logcumsumexp(x)
+
+    # Assert that the result equals the expected result
+    assert_true(np.array_equal(result.data, expected_result.data))
+
+
+def test_backward():
+
+    # Construct test data
+    x = Variable(np.array([5., 3., 3., 1., 0.]))
+    g = Variable(np.ones(5))
+    expected_result = np.array([0.7717692057972512, 0.562087881852882,
+                                1.4058826163342215, 0.9213241007090265,
+                                1.3389361953066183])
+
+    # Generate object
+    lcse = LogCumsumExp()
+
+    # Run forward and backward pass
+    lcse.forward((x.data,))
+    result = lcse.backward((x.data, ), (g.data, ))
+
+    # Assert that the result equals the expected result
+    assert_true(np.array_equal(result[0], expected_result))
---
+++ test/loss/test_listwise.py
@@ -0,0 +1,75 @@
+import numpy as np
+from nose.tools import raises, assert_equal, assert_almost_equal
+from chainer import Variable, Link
+from lace.loss.listwise import ListNetLoss, ListMLELoss, ListPLLoss, \
+    AbstractListLoss
+
+
+class Identity(Link):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def __call__(self, x):
+        return x
+
+
+@raises(NotImplementedError)
+def test_abstract_notimplemented():
+    x = np.array([[3., 3., 2., 0.]]).T
+    t = np.array([[3., 2., 0., 1.]]).T
+    loss = AbstractListLoss(Identity())
+    loss(x, t)
+
+
+def test_listnet():
+    x = np.array([[3., 3., 2., 0.]]).T
+    t = np.array([[0.5, 1.0, 0.3, 0.5]]).T
+    loss = ListNetLoss(Identity())
+
+    result = loss(x, t)
+    assert_equal(result.data, 0.43439806229182915)
+
+
+def test_listnet_near_zero_loss():
+    x = np.array([[600., 400., 200., 0.]]).T
+    t = np.array([[600., 400., 200., 0.]]).T
+    loss = ListNetLoss(Identity())
+
+    result = loss(x, t)
+    assert_almost_equal(result.data, 0.0)
+
+
+def test_listmle():
+    x = np.array([[3., 3., 2., 0.]]).T
+    t = np.array([[0.5, 1.0, 0.3, 0.5]]).T
+    loss = ListMLELoss(Identity())
+
+    result = loss(x, t)
+    assert_equal(result.data, 1.3587430505329978)
+
+
+def test_listmle_near_zero_loss():
+    x = np.array([[600., 400., 200., 0.]]).T
+    t = np.array([[600., 400., 200., 0.]]).T
+    loss = ListMLELoss(Identity())
+
+    result = loss(x, t)
+    assert_almost_equal(result.data, 0.0)
+
+
+def test_listpl():
+    x = np.array([[3., 3., 2., 0.]]).T
+    t = np.array([[0.5, 1.0, 0.3, 0.5]]).T
+    loss = ListPLLoss(Identity())
+
+    result = loss(x, t)
+    assert_equal(result.data, 5.545076727008247)
+
+
+def test_listpl_near_zero_loss():
+    x = np.array([[40., 20., 0.]]).T
+    t = np.array([[40., 20., 0.]]).T
+    loss = ListPLLoss(Identity())
+
+    result = loss(x, t)
+    assert_almost_equal(result.data, 0.0)
